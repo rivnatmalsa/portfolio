fc389e16b376158f9846270a822f22f750ac942c
Tanvir Aslam Mohammed <rivnatmalsa@gmail.com>
2017-05-09 18:36:47 -0700

    Add temporary under construction alerts for blogs and project buttons.

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -1,10 +1,27 @@
 import rotateTitle from "rotateTitle";
+import "../../node_modules/sweetalert/dist/sweetalert.min.js";
 
 const initialize = ()=>{
     //Initialize all tooltips
     $('[data-toggle="tooltip"]').tooltip();
 
     rotateTitle();
+
+    // Temporary code -- Removed after blog and project sections are up.
+    const btns = document.querySelectorAll('.btn-custom');
+    btns.forEach(btn=>{
+        btn.addEventListener('click', ()=>{
+            swal({
+                title: "Under Construction",
+                text: "This section is under construction! Will be up soon!",
+                type: "info",
+                showCancelButton: true,
+                closeOnConfirm: false,
+                showLoaderOnConfirm: true,
+                allowEscapeKey: true
+            })
+        });
+    });
 }
 
 export default initialize;
